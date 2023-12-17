806aadd9f1541cee83f4a2169c0f86cfc146d0b6
PatrikTison <patriktison@gmail.com>
2023-12-17 15:43:44 +0100

    app.py was updated

--- app.py
+++ app.py
@@ -1,39 +1,35 @@
-import time
-
-import cv2
 import streamlit as st
-from PIL import Image
-from supervision.utils.video import get_video_frames_generator
 
 from src.video_player import VideoPlayer
 from src.video_utils import get_video_fps, save_temporary_video
 
-player = VideoPlayer()
 
-uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
+def main():
+    player = VideoPlayer()
+    uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
 
-# Initialize or get the state variable for replay
-if "replay" not in st.session_state:
-    st.session_state.replay = False
+    if uploaded_file is not None:
+        # Detect changes in the uploaded file
+        if (
+            "last_uploaded_file" not in st.session_state
+            or st.session_state.last_uploaded_file != uploaded_file.name
+        ):
+            st.session_state.last_uploaded_file = uploaded_file.name
+            st.session_state.replay = True  # Set replay to True for new file
+
+        st.write("Uploaded video")
+        handle_replay(player, uploaded_file)
 
 
-def play_video():
-    st.session_state.replay = True
+def handle_replay(player, uploaded_file):
+    replay_button_clicked = st.button("Replay Video")
 
-
-if uploaded_file is not None:
-    st.write("Uploaded video")
-
-    # Button to replay the video
-    if st.button("Replay Video"):
-        play_video()
-
-    file_path = save_temporary_video(uploaded_file)
-    time_per_frame = get_video_fps(file_path)
-
-    # Play the video initially or when replay is triggered
-    if not st.session_state.replay:
+    if replay_button_clicked or st.session_state.replay:
+        file_path = save_temporary_video(uploaded_file)
+        time_per_frame = get_video_fps(file_path)
         player.display_video_frames(file_path, time_per_frame)
-    elif st.session_state.replay:
-        st.session_state.replay = False  # Reset the replay state
-        player.display_video_frames(file_path, time_per_frame)
+        st.session_state.replay = False
+
+
+if __name__ == "__main__":
+    main()
