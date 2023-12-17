58545209a648cec19d276cc6899aab1b7fd2899f
PatrikTison <patriktison@gmail.com>
2023-12-17 17:31:03 +0100

    first working version

--- src/processor.py
+++ src/processor.py
@@ -11,23 +11,18 @@ class MediaProcessor:
 
     def process_video_frame(self, frame):
         results = self.model(frame)
-        box_annotator = sv.BoxAnnotator(
+        box_annotator = sv.BoundingBoxAnnotator(
             color=ColorPalette.default(),  # Use the default color palette
             thickness=4,
-            text_thickness=4,
-            text_scale=2,
         )
         detections = sv.Detections(
             xyxy=results[0].boxes.xyxy.cpu().numpy(),
             confidence=results[0].boxes.conf.cpu().numpy(),
             class_id=results[0].boxes.cls.cpu().numpy().astype(int),
         )
-        processed_frame = frame
-        # labels = [
-        #     f"{self.class_names_dict[class_id]} {confidence:0.2f}"
-        #     for _, confidence, class_id, tracker_id in detections
-        # ]
-        # processed_frame = box_annotator.annotate(
-        #     frame=frame, detections=detections, labels=labels
-        # )
+        labels = [
+            f"{self.class_names_dict[class_id]} {conf:0.2f}"
+            for class_id, conf in zip(detections.class_id, detections.confidence)
+        ]
+        processed_frame = box_annotator.annotate(scene=frame, detections=detections)
         return processed_frame
