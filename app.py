960010e54333691669c9fa32c51a9db654747d9a
PatrikTison <patriktison@gmail.com>
2023-12-18 20:36:50 +0100

    menu was updated

--- app.py
+++ app.py
@@ -7,19 +7,40 @@ from src.video_utils import save_temporary_video
 def main():
     st.sidebar.title("Menu")
 
+    # Initialize the current page in session state
+    if "current_page" not in st.session_state:
+        st.session_state.current_page = "Analyze"
+
+    # Sidebar buttons for page navigation
+    if st.sidebar.button("Analyze"):
+        st.session_state.current_page = "Analyze"
+    if st.sidebar.button("Train"):
+        st.session_state.current_page = "Train"
+
+    # Call the appropriate page function based on current page
+    if st.session_state.current_page == "Analyze":
+        analyze_page()
+    elif st.session_state.current_page == "Train":
+        train_page()
+
+
+def analyze_page():
     uploaded_file = st.file_uploader("Choose a video...", type=["mp4"])
 
     if uploaded_file is not None:
-        # Detect changes in the uploaded file
         if (
             "last_uploaded_file" not in st.session_state
             or st.session_state.last_uploaded_file != uploaded_file.name
         ):
             st.session_state.last_uploaded_file = uploaded_file.name
-            st.session_state.replay = True  # Set replay to True for new file
+            st.session_state.replay = True
 
         st.write("Uploaded video")
         handle_replay(uploaded_file)
+
+
+def train_page():
+    st.write("Train your model here.")
 
 
 def handle_replay(uploaded_file):
