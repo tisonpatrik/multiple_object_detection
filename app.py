2afccb9c144935a2cf7c52461a70feedbe1fb5f1
PatrikTison <patriktison@gmail.com>
2023-12-19 11:36:13 +0100

    small update of app.py

--- app.py
+++ app.py
@@ -1,28 +1,29 @@
 import streamlit as st
 
-from src.handlers.replay_handler import ReplayHandler
 from src.pages.analyze_page import AnalyzePage
 from src.pages.train_page import TrainPage
 
-replay_handler = ReplayHandler()
-
 
 def main():
-    st.sidebar.title("Menu")
+    # Sidebar setup using 'with' notation
+    with st.sidebar:
+        st.title("Menu")
 
-    # Initialize the current page in session state
-    if "current_page" not in st.session_state:
-        st.session_state.current_page = "Analyze"
+        # Initialize the current page in session state
+        if "current_page" not in st.session_state:
+            st.session_state.current_page = "Analyze"
 
-    # Sidebar buttons for page navigation
-    if st.sidebar.button("Analyze"):
-        st.session_state.current_page = "Analyze"
-    if st.sidebar.button("Train"):
-        st.session_state.current_page = "Train"
+        # Sidebar buttons for page navigation
+        if st.button("Analyze"):
+            st.session_state.current_page = "Analyze"
+        if st.button("Train"):
+            st.session_state.current_page = "Train"
 
+    # Instantiate page classes
     analyze_page = AnalyzePage()
     train_page = TrainPage()
-    # Call the appropriate page function based on current page
+
+    # Render the appropriate page based on current page
     if st.session_state.current_page == "Analyze":
         analyze_page.render()
     elif st.session_state.current_page == "Train":
