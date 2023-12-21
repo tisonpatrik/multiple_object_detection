161ed68648ccbced7b09ceeb92321c1957ce9096
PatrikTison <patriktison@gmail.com>
2023-12-22 00:48:19 +0100

    error handling added

--- src/handlers/replay_handler.py
+++ src/handlers/replay_handler.py
@@ -1,7 +1,13 @@
+import logging
+
 import streamlit as st
 
 from src.video.video_player import VideoPlayer
 from src.video.video_utils import save_temporary_video
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 
 class ReplayHandler:
@@ -10,9 +16,13 @@ class ReplayHandler:
         self.frame_placeholder = st.empty()
 
     def handle_replay(self, uploaded_file):
-        replay_button_clicked = st.button("Replay Video")
+        try:
+            replay_button_clicked = st.button("Replay Video")
 
-        if replay_button_clicked or st.session_state.replay:
-            file_path = save_temporary_video(uploaded_file)
-            self.player.display_video_frames(file_path)
-            st.session_state.replay = False
+            if replay_button_clicked or st.session_state.replay:
+                file_path = save_temporary_video(uploaded_file)
+                self.player.display_video_frames(file_path)
+                st.session_state.replay = False
+        except Exception as e:
+            logger.error(f"Error handling replay: {e}")
+            raise
